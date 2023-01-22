c16ea4438691350837a0dd2543a3d98af4f234d6
Einzel <einzel0102@mail.ru>
2023-01-22 13:25:37 +0800

    задача 42 через строку string

---
+++ Task42.1.string/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+string ConvertDecToBin(int num)
+{
+    string result = string.Empty;
+    while (num > 0)
+    {
+        result = num % 2 + result;
+        num = num / 2;
+    }
+    return result;
+}
+
+Console.WriteLine("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string res = ConvertDecToBin(number);
+Console.WriteLine(res);
