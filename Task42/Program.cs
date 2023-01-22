eb1b75d5860807433c244d0e675cba110d550626
Einzel <einzel0102@mail.ru>
2023-01-22 13:39:08 +0800

    попытка решить задачу 42 через рекурсию

---
+++ Task42/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+// double Factorial1(int n)
+// {
+//  if(n == 1) return 1;
+//  else return n * Factorial1(n-1);
+// }
+// Console.WriteLine(Factorial1(4));
+
+int Remainder(int num)
+{
+    if (num % 2 == 1) return 1;
+    else return Remainder(num / 2);
+}
+Console.WriteLine(Remainder(12));
+
+
+
